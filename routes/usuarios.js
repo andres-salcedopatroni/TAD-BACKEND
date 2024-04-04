113eeaeec09273178690d9fce33c4529039ae96b
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-04-04 16:20:31 -0500

    Actualizacion

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -29,5 +29,53 @@ router.get('/obtener/:codigo', async function(req, res, next) {
   }
 });
 
+router.post('/agregar', 
+  async function(req, res, next) {
+    const fecha = new Date();
+    const lista_usuarios=await usuarios.find({});
+    const pedido = req.body;
+    try{
+      const e = new usuarios({
+        nombre: pedido.nombre, 
+        codigo: lista_usuarios.length,
+        clave: pedido.usuario,
+        correo: pedido.correo,
+        celular: pedido.celular,
+        fecha: fecha,
+      });
+      await e.save();
+      res.json({'mensaje':'Usuario agregado'});
+    }
+    catch(error){
+      res.status(400).json({'mensaje':error});
+    };
+  }
+);
+
+router.delete('/eliminar', async function(req, res, next) {
+  try{
+    const pedido=req.body;
+    await usuarios.deleteMany({codigo: pedido.codigo});
+    res.json({"mensaje": "Estudiante eliminado"});
+  }
+  catch(error){
+    res.status(400).json({'mensaje':error})
+  }
+});
+
+router.put('/actualizar', async function(req, res, next) {
+  try{
+    const pedido=req.body;
+    const e = await usuarios.findOne({codigo: pedido.codigo});
+    e.nombre = pedido.nombre;
+    e.correo = pedido.correo;
+    e.celular = pedido.celular;
+    await e.save();
+    res.json({"mensaje": "Estudiante actualizado"});
+  }
+  catch(error){
+    res.status(400).json({'mensaje':error})
+  }
+});
 
 module.exports = router;
--- schemas/schema_usuarios.js
+++ schemas/schema_usuarios.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const schema_usuarios= new Schema(
     { 
         nombre: String, 
-        codigo: String,
+        codigo: {type: String, unique: true, required: true},
         clave: {type: String, required: true},
         correo: {type: String, unique: true, required: true},
         celular: {type: Number, unique: true, required: true},
