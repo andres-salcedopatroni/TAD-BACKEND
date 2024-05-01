66f5fc310e6eabec4430de8b420d09b756d79e38
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-05-01 15:47:08 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
