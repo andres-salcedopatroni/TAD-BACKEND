53e443c882e786b0e0e3d3d0170a86183e066bc5
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-04-30 20:40:21 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const usuariosRouter = require('./routes/usuarios');
 const productosRouter = require('./routes/productos');
 const ventasRouter = require('./routes/ventas');
 const cors = require('cors')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 //Conexion Atlas
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://'+Usuario+':'+Clave+'@'+CLUSTER+'/'+BD+'?retryWrites=true&w=majority');
@@ -31,24 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({ origin: '*' }));
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use('/', indexRouter);
