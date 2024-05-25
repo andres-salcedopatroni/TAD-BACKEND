f0ad02efdf696382e808affe0aa2c6ed9825658d
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-05-24 20:25:42 -0500

    Comentario

---
+++ routes/comentarios.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+//Mongo
+const mongoose = require('mongoose');
+const schema_comentarios= require('../schemas/schema_comentarios');
+const comentarios = mongoose.model('Ventas', schema_comentarios,'Ventas');
+
+//Comentarios de producto
+router.get('/comentarios_producto/:nombre&:codigo_productor', async function(req, res, next) {
+  try{
+    const nombre = req.params.nombre;
+    const codigo_productor = req.params.codigo_productor;
+    const lista_comentarios = await comentarios.find({nombre_producto: nombre,codigo_productor: codigo_productor});
+    res.json(lista_comentarios);
+  }
+  catch(error){
+    res.status(400).json({'mensaje':error});
+  }
+});
+
+//Registrar comentario
+router.post('/registrar_ventas', async function(req, res, next) {
+
+    try{
+        const fecha = new Date();
+        const comentario_datos = req.body;
+        let comentario = new comentarios({
+            contenido: comentario_datos.contenido, 
+            nombre_usuario: comentario_datos.nombre_usuario, 
+            codigo_usuario: comentario_datos.codigo_usuario,
+            nombre_producto: comentario_datos.nombre_producto,
+            codigo_productor: comentario_datos.codigo_productor,
+            fecha: fecha
+        });
+        await comentario.save();
+        res.json({'mensaje':'Comentario registrado'});
+    }
+    catch(error){
+    res.status(400).json({'mensaje':error});
+  };
+});
+
+module.exports = router;
--- schemas/schema_comentarios.js
+++ schemas/schema_comentarios.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const schema_comentarios= new Schema(
     { 
         contenido: String, 
-        nombreUsuario: String, 
+        nombre_usuario: String, 
         codigo_usuario: String,
         nombre_producto: String,
         codigo_productor: String,
